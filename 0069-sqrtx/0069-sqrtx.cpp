13d278c6194edbd6f87137861c756d8070cdcfc8
SAURABH KUMAR SINGH <95154207+saurabhhsinghh@users.noreply.github.com>
2023-12-10 22:11:05 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (71.67%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,31 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        if(x<2)
+            return x;
+        int s=1,e=x,mid,index=0;
         
-        double a=sqrt(x);
-        int b=a;
-        return b;
+        while(s<=e)
+        {
+            mid=s+(e-s)/2;
+            if(mid==x/mid)
+            {
+                index=mid;
+                break;
+            }
+                
+            else if(mid<x/mid)
+            {
+                s=mid+1;
+                index=mid;
+            }
+            else
+            {
+                e=mid-1;
+                index=mid-1;
+            }
+        }
+        return index;
         
     }
 };
