33bb766a52898595f8cbde8d68c0056de11668ec
SAURABH KUMAR SINGH <95154207+saurabhhsinghh@users.noreply.github.com>
2024-06-26 09:54:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<long long> lcmAndGcd(long long A , long long B) {
+        // code here
+        long long g= __gcd(A,B);
+        long long lcm=(A*B)/g;
+        vector<long long> v;
+        v.push_back(lcm);
+        v.push_back(g);
+        return v;
+        
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long A,B;
+        
+        cin>>A>>B;
+
+        Solution ob;
+        vector<long long> ans = ob.lcmAndGcd(A,B);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
